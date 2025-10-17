1abe325d7e7189ecb4d9412da971712d33579e1e
한재호 <wogh009@gmail.com>
2025-10-17 10:45:21 +0900

    배포 테스트

--- public/libs/config.js
+++ public/libs/config.js
@@ -1,1 +1,4 @@
-const API_BASE_URL = "http://localhost:3000";
+// 배포 환경에 따라 API_BASE_URL 자동 설정
+const API_BASE_URL = window.location.hostname === 'localhost'
+  ? 'http://localhost:3000'
+  : window.location.origin; // 배포 시 현재 도메인 사용
